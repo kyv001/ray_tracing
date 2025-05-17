7b62927130a5cbddafe28bd4ea3ba61e5808bdb0
kyv001 <kyvyang@foxmail.com>
2025-05-17 14:15:44 +0800

    支持多进程渲染

--- ray_tracing/camera.py
+++ ray_tracing/camera.py
@@ -1,6 +1,7 @@
 from pygame.math import *
 from others import *
 from tqdm import tqdm
+import multiprocessing
 
 class Camera:
     def __init__(self, x, y, z):
@@ -10,18 +11,41 @@ class Camera:
 
     def render(self, screen, things):
         color = screen.pos * 1
-        for x in tqdm(range(screen.length)):
-            for y in range(screen.height):
-                d = (screen.pos[y][x] - Vector3(self.x, self.y, self.z)).normalize()
-                d_last = inf
-                for thing in things:
-                    c, distance = thing.get_color(Vector3(self.x, self.y, self.z), d, things, 0)
-                    if distance < d_last:
-                        color[y][x] = list(c)
-                        d_last = distance
-                
+        q = multiprocessing.Queue(maxsize=1024)
+        def _render(offset, size, screen, things, q):
+            for x in tqdm(range(offset[0], offset[0] + size[0])):
+                for y in range(offset[1], offset[1] + size[1]):
+                    d = (screen.pos[y][x] - Vector3(self.x, self.y, self.z)).normalize()
+                    d_last = inf
+                    for thing in things:
+                        c, distance = thing.get_color(Vector3(self.x, self.y, self.z), d, things, 0)
+                        if distance < d_last:
+                            q.put((x, y, list(c)))
+                            d_last = distance
+            q.put(None)
+        
+        processes = []
+        n_groups = 64
+        assert (n_groups ** 0.5).is_integer()
+        n_xgoups = n_ygoups = int(n_groups ** 0.5)
+        for ia in range(n_xgoups):
+            for ib in range(n_ygoups):
+                offset = [ia * screen.length // n_xgoups, ib * screen.height // n_xgoups]
+                size = [screen.length // n_xgoups, screen.height // n_xgoups]
+                process = multiprocessing.Process(target=_render, args=(offset, size, screen, things, q))
+                process.start()
+                processes.append(process)
+        
+        finish_count = 0
+        while finish_count < n_groups:
+            d = q.get()
+            if d is None:
+                finish_count += 1
+            else:
+                (x, y, c) = d
                 for i in range(3):
-                    color[y][x][i] = max(color[y][x][i], 0)
+                    c[i] = max(0, c[i])
+                color[y][x] = c
 
         m = 0
         for x in range(screen.length):
